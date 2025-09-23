6be18d8e0e25a43753e823a4c4c1a48e782e1f69
Nishxsm <nishantdandnaik156@gmail.com>
2025-09-24 00:00:30 +0530

    Frontend changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./components/landingpage";
-// import Login from "./components/Login";
-// import Signup from "./components/Signup";
+import Login from "./components/login";
+import Signup from "./components/signup";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      {/* <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} /> */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 }
