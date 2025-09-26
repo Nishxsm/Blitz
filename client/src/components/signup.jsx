1a1fd78851166e3706d78b5441c35e56218cc47c
Nishxsm <nishantdandnaik156@gmail.com>
2025-09-26 23:16:01 +0530

    auth work

--- client/src/components/signup.jsx
+++ client/src/components/signup.jsx
@@ -1,3 +1,39 @@
+import { useState } from "react";
+
+
 export default function signup(){
-    return<h2>Signup</h2>;
+    const [form,setForm]=useState({name:"",email:"",password:""});
+    const [error,setError]=useState("");
+    const [success,setSuccess]=useState(false);
+
+    function handlechange(e){
+        setForm({...form,[e.target.name]:e.target.value });
+    }
+
+    async function handleSubmit(e){
+        e.preventDefault();
+        setError("");
+        setSuccess(false);
+
+        const res=await fetch("http://localhost:4000/api/auth/signup",{
+            method:"POST",
+            credentials:"include",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(form)
+        });
+
+        const data=await res.json();
+        if(!res.ok)setError(data.message || "Singup failed.");
+        else setSuccess(true);
+    }
+
+    return(
+        <div className="auth-page">
+            <h2>Sign-Up</h2>
+            <form onSubmit={handleSubmit}>
+                <input name="name" placeholder="Name" value={form.name} onChange={handlechange} required />
+                <input name="email" type="email" placeholder="Email" value={form.email} onChange={handlechange} required />
+            </form>
+        </div>
+    )
 }
